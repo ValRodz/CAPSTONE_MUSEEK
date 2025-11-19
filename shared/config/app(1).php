d3b37f3eb4c45dece0289492df3f74f445f9749d
Valerie Odette Rodriguez <vogrodriguez.chmsu@gmail.com>
2025-11-19 13:13:23 +0800

    Add files via upload

---
+++ shared/config/app(1).php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Application-level overrides.
+ * Set 'app_url' to the publicly accessible base URL so emailed links work outside localhost.
+ */
+return [
+    // e.g. 'https://museek.yourdomain.com' or 'http://192.168.1.50/museek'
+    'app_url' => '',
+];
+
